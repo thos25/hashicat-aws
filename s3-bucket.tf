9be93220a81bf6833e0a71767e47dcab41d3cff3
Joey Axtell <joey.axtell@gmail.com>
2021-01-14 21:42:25 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -2,5 +2,5 @@ module "s3-bucket" {
   source  = "app.terraform.io/Thos25/s3-bucket/aws"
   version = "1.17.0"
   bucket_prefix = "joeyaxtell"
-  # insert required variables here
+  # insert required variables here test1
 }
